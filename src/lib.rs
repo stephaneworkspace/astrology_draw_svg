5cec84930e79e2053c218db9a22a1064f94278b9
stephaneworkspace <s.bressani@bluewin.ch>
2020-01-01 14:52:03 +0100

    Initial commit

---
+++ src/lib.rs
@@ -0,0 +1,31 @@
+use svg::node::element::path::Data;
+use svg::node::element::Path;
+use svg::Document;
+
+pub extern "C" fn write() -> String {
+    let data = Data::new()
+        .move_to((10, 10))
+        .line_by((0, 50))
+        .line_by((50, 0))
+        .line_by((0, -50))
+        .close();
+
+    let path = Path::new()
+        .set("fill", "none")
+        .set("stroke", "black")
+        .set("stroke-width", 3)
+        .set("d", data);
+
+    let document = Document::new().set("viewBox", (0, 0, 70, 70)).add(path);
+
+    //svg::save("image.svg", &document).unwrap();
+    document.to_string()
+}
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn it_works() {
+        assert_eq!(2 + 2, 4);
+    }
+}
